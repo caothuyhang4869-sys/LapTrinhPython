b0ee26b2f7f51386caf105cce6931e3cf6846dd6
Thuy Hang <caothuyhang4869@gmail.com>
2026-04-17 14:16:09 +0700

    Tổng và tích các chữ số trong số n

---
+++ Tong_tich.py
@@ -0,0 +1,13 @@
+n = int(input("Nhập số nguyên dương n: "))
+
+tong = 0
+tich = 1
+
+while n > 0:
+        t = n % 10
+        tong += t
+        tich *= t
+        n //= 10
+    
+print("Tổng = ",tong)
+print("Tích =  ",tich)
